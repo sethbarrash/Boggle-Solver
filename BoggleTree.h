479c6580c7c012e056a3a717344e6151cc971ebb
sethbarrash <sethbarrash@gmail.com>
2022-07-21 17:59:57 -0500

    Add BoggleTree.h

---
+++ BoggleTree.h
@@ -0,0 +1,44 @@
+#include "WordTree.h"
+
+typedef BoggleNode* BoggleNodePtr;
+
+struct BoggleNode {
+  char letter;
+  int x;
+  int y;
+  char tentative_word [17];
+  BoggleNode* children [8];
+  BoggleNode* parent;
+  Node* branch;
+  bool is_end_of_word;
+};
+
+bool is_ancestor(BoggleNodePtr b, int x0, int y0);
+
+class BoggleTree {
+  public:
+    BoggleNodePtr root;
+    void delete_subtree(BoggleNodePtr t);
+    BoggleTree();
+    ~BoggleTree();
+};
+
+bool is_ancestor(BoggleNodePtr b, int x0, int y0) {
+  while (b->parent) {
+    b = b->parent;
+    if (b->x == x0 && b->y == y0) return 1;
+  }
+  return 0;
+}
+
+void BoggleTree::delete_subtree(BoggleNodePtr t) {
+  for (int k=0; k<8; k++) delete_subtree(t->children[k]);
+}
+
+BoggleTree::BoggleTree() {
+  root = NULL;
+}
+
+BoggleTree::~BoggleTree() {
+  delete_subtree(root);
+}
