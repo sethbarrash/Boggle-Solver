9fe4f5c54be17482b01a78e9c21ea0a12de64e9c
sethbarrash <sethbarrash@gmail.com>
2024-05-14 20:29:54 -0500

    Rewrite tests as invocations of test_board

--- test/test_BoggleSolver.cpp
+++ test/test_BoggleSolver.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <cassert>
 #include <iostream>
 #include "BoggleSolver.h"
@@ -6,60 +7,43 @@
 
 using std::cout;
 
-void test_board0(const VocabTree t)
+void test_board(char board [][4], word_set expected_words, const VocabTree t)
 {
-    char board0[4][4] = {
-        {'a', 'r', 'g', 't'},
-        {'n', 'e', 'v', 'r'},
-        {'d', 'f', 'b', 'a'},
-        {'t', 'w', 'e', 'n'}
-    };
-
-    BoggleSolver bs = BoggleSolver(board0, t);
+    BoggleSolver bs = BoggleSolver(board, t);
     word_set ws = bs.solve_board();
-    assert(ws == ws3);
-}
+    if (ws != expected_words)
+    {
+        vector<string> missing_from_found;
+        vector<string> missing_from_expected;
+        vector<string>::iterator i, j;
+        i = set_difference(
+            ws.begin(),
+            ws.end(),
+            expected_words.begin(),
+            expected_words.end(),
+            missing_from_found.begin()
+        );
 
-void test_board1(const VocabTree t)
-{
-    char board1[4][4] = {
-        {'e', 'e', 'p', 't'},
-        {'a', 'r', 'a', 'u'},
-        {'w', 't', 'y', 'u'},
-        {'c', 'y', 'j', 'l'}
-    };
+        j = set_difference(
+            expected_words.begin(),
+            expected_words.end(),
+            ws.begin(),
+            ws.end(),
+            missing_from_expected.begin()
+        );
 
-    BoggleSolver bs = BoggleSolver(board1, t);
-    word_set ws = bs.solve_board();
-    assert(ws == ws3);
-}
-
-void test_board2(const VocabTree t)
-{
-    char board2[4][4] = {
-        {'w', 'a', 't', 'u'},
-        {'u', 'i', 'g', 'b'},
-        {'h', 't', 'y', 'z'},
-        {'e', 'i', 'd', 'o'}
-    };
-
-    BoggleSolver bs = BoggleSolver(board2, t);
-    word_set ws = bs.solve_board();
-    assert(ws == ws2);
-}
-
-void test_board3(const VocabTree t)
-{
-    char board3[4][4] = {
-        {'n', 't', 'o', 'b'},
-        {'f', 'a', 'x', 'm'},
-        {'r', 'o', 'e', 'g'},
-        {'r', 'n', 'h', 's'}
-    };
-
-    BoggleSolver bs = BoggleSolver(board3, t);
-    word_set ws = bs.solve_board();
-    assert(ws == ws3);
+        cout << "The following words were expected but not found by the BoggleSolver:\n";
+        for (i = missing_from_found.begin(); i != missing_from_found.end(); i++)
+        {
+            if (!(*i).empty()) cout << *i << '\n';
+        }
+        
+        cout << "The following words were found by the BoggleSolver but not expected:\n";
+        for (j = missing_from_expected.begin(); j != missing_from_expected.end(); j++)
+        {
+            if (!(*j).empty()) cout << *j << '\n';
+        }
+    }
 }
 
 int main()
@@ -67,10 +51,10 @@ int main()
     char input_file [100] = "boggleWords.txt";
     VocabTree t = VocabTree(input_file);
 
-    test_board0(t);
-    test_board1(t);
-    test_board2(t);
-    test_board3(t);
+    test_board(board0, ws0, t);
+    test_board(board1, ws1, t);
+    test_board(board2, ws2, t);
+    test_board(board3, ws3, t);
 
     return 0;
 }
