bbccac31fa59b169f4b0535879cc3ec73b227985
sethbarrash <sethbarrash@gmail.com>
2024-05-19 20:57:09 -0500

    Change data for test_binary_search

--- test/test_binary_search.cpp
+++ test/test_binary_search.cpp
@@ -1,26 +1,31 @@
 #include <cassert>
+#include <iostream>
+#include <cstring>
 #include "binary_search.h"
+#include "BoggleTree.h"
+
+using std::cout;
 
 int main() {
-  char genesis     [MAX_WORD_LENGTH] = "genesis";
-  char exodus      [MAX_WORD_LENGTH] = "exodus";
-  char leviticus   [MAX_WORD_LENGTH] = "leviticus";
-  char numbers     [MAX_WORD_LENGTH] = "numbers";
-  char deuteronomy [MAX_WORD_LENGTH] = "deuteronomy";
-  char joshua      [MAX_WORD_LENGTH] = "joshua";
+  char new_york   [MAX_WORD_LENGTH] = "New York";
+  char baltimore  [MAX_WORD_LENGTH] = "Baltimore";
+  char chicago    [MAX_WORD_LENGTH] = "Chicago";
+  char cleveland  [MAX_WORD_LENGTH] = "Cleveland";
+  char washington [MAX_WORD_LENGTH] = "Washington";
+  char detroit    [MAX_WORD_LENGTH] = "Detroit";
 
   char a [5][MAX_WORD_LENGTH] = {
-    "deuteronomy",
-    "exodus",
-    "genesis",
-    "leviticus",
-    "numbers"
+    "New York",
+    "Baltimore",
+    "Chicago",
+    "Cleveland",
+    "Washington"
   };
 
-  assert(binary_search(a,joshua,5)      == -1);
-  assert(binary_search(a,deuteronomy,5) == 0);
-  assert(binary_search(a,exodus,5)      == 1);
-  assert(binary_search(a,genesis,5)     == 2);
-  assert(binary_search(a,leviticus,5)   == 3);
-  assert(binary_search(a,numbers,5)     == 4);
+  assert(binary_search(a, detroit, 5)    == -1);
+  assert(binary_search(a, new_york, 5)   == -1);
+  assert(binary_search(a, chicago, 5)    == 2);
+  assert(binary_search(a, cleveland, 5)  == 3);
+  assert(binary_search(a, washington, 5) == 4);
+  assert(binary_search(a, baltimore, 5)  == -1);
 }
