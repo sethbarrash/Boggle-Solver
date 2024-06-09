a30b30acdb1b99fa25070409ecc076b8480fc1fe
sethbarrash <sethbarrash@gmail.com>
2024-06-08 21:00:28 -0500

    Fix test_mergesort

--- test/test_mergesort.cpp
+++ test/test_mergesort.cpp
@@ -1,13 +1,6 @@
 #include <cassert>
 #include <cstring>
-#include <iostream>
 #include "mergesort.h"
-
-void print_words(char (*a)[MAX_WORD_LENGTH], int length) {
-  for (int i=0; i<length; i++) {
-    cout << a[i] << '\n';
-  }
-}
 
 int main() {
   char new_york   [MAX_WORD_LENGTH] = "New York";
@@ -26,11 +19,10 @@ int main() {
   };
 
   mergesort(a,5);
-  print_words(a,5);
 
-  assert(!strcmp(a[0],baltimore));
-  assert(!strcmp(a[1],chicago));
-  assert(!strcmp(a[2],cleveland));
-  assert(!strcmp(a[3],detroit));
-  assert(!strcmp(a[4],washington));
+  assert(!strcmp(a[0], baltimore));
+  assert(!strcmp(a[1], chicago));
+  assert(!strcmp(a[2], cleveland));
+  assert(!strcmp(a[3], new_york));
+  assert(!strcmp(a[4], washington));
 }
