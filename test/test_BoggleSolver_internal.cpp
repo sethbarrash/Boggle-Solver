ce1eac6f7b94fd47e2a3b4fbfe20d6879d2a264e
sethbarrash <sethbarrash@gmail.com>
2024-06-13 13:41:24 -0500

    Write and debug test_make_step

--- test/test_BoggleSolver_internal.cpp
+++ test/test_BoggleSolver_internal.cpp
@@ -38,7 +38,7 @@ void test_is_not_on_board() {
     assert(is_not_on_board(4, 4) == 1);
 }
 
-void test_make_first_step_0() {
+void test_make_step() {
     BoggleSolver bs(board0);
     assert(bs.ws.empty());
     assert(bs.q.empty());
@@ -67,9 +67,39 @@ void test_make_first_step_0() {
     assert(step_0_1.word_so_far == "ar");
     assert(step_0_1.squares_used == expected_squares_used);
     assert(bs.ws.empty());
+
+    SolutionStep step_0_2 = bs.make_step(0, 2, step_0_1);
+    set<pair<uint8_t, uint8_t>> expected_squares_0_2 = expected_squares_used;
+    expected_squares_0_2.insert({0, 2});
+
+    assert(bs.could_lead_to_new_words(0, 2, step_0_1));
+
+    assert(step_0_2.x == 0);
+    assert(step_0_2.y == 2);
+    assert(step_0_2.depth == 2);
+    assert(step_0_2.v);
+    assert(step_0_2.word_so_far == "arg");
+    assert(step_0_2.squares_used == expected_squares_0_2);
+    assert(bs.ws.size() == 1);
+    assert(bs.ws.count("arg") == 1);
+
+    SolutionStep step_1_1 = bs.make_step(1, 1, step_0_1);
+    set<pair<uint8_t, uint8_t>> expected_squares_1_1 = expected_squares_used;
+    expected_squares_1_1.insert({1, 1});
+
+    assert(bs.could_lead_to_new_words(1, 1, step_0_1));
+
+    assert(step_1_1.x == 1);
+    assert(step_1_1.y == 1);
+    assert(step_1_1.depth == 2);
+    assert(step_1_1.v);
+    assert(step_1_1.word_so_far == "are");
+    assert(step_1_1.squares_used == expected_squares_1_1);
+    assert(bs.ws.size() == 2);
+    assert(bs.ws.count("are") == 1);
 }
 
 int main() {
     test_is_not_on_board();
-    test_make_first_step_0();
+    test_make_first_steps_0();
 }
